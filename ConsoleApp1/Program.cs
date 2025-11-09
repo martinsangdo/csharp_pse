48e7a55b0fbc0c624098369b2737165a654e5317
Martin Sang Do <dothanhsang1984@yahoo.com>
2025-11-10 02:06:22 +0700

    - print name

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World 4!");
+﻿Console.WriteLine("What is your name?");
+var name = Console.ReadLine();
+var currentDate = DateTime.Now;
+Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+Console.Write($"{Environment.NewLine}Press Enter to exit...");
+Console.Read();
---
+++ basic/Hello.cs
@@ -0,0 +1,12 @@
+using System;
+//this is old .net 5
+// namespace HelloWorld
+// {
+//     internal class Program
+//     {
+//         static void Main(string[] args)
+//         {
+            Console.WriteLine("Hello, World!");
+//         }
+//     }
+// }
