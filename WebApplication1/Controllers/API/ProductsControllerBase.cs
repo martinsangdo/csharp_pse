e279366f130716fa9b9b26bb2920d63d0d994040
Martin Sang Do <dothanhsang1984@yahoo.com>
2025-11-15 16:32:03 +0700

    - GET APIs with params

--- WebApplication1/Controllers/API/ProductsControllerBase.cs
+++ WebApplication1/Controllers/API/ProductsControllerBase.cs
@@ -10,4 +10,28 @@ public class ProductsControllerBase : ControllerBase
         string receivedName = "Received: " + name;
         return Ok(receivedName);    //
     }
+
+    [HttpGet]
+    [Route("{id}")]
+    public IActionResult productDetail(int id)
+    {
+        string response = "Received product ID: " + id;
+        return Ok(response);
+    }
+
+    [HttpGet]
+    [Route("{id}/category/{catID}")]
+    public IActionResult productDetailWCat(int id, int catID)
+    {
+        string response = "Received product ID: " + id + " catId: " + catID;
+        return Ok(response);
+    }
+
+    [HttpGet]
+    [Route("discount/{product_id}")]
+    public IActionResult getDiscount(int product_id, int cat_id)
+    {
+        string response = "Received product ID: " + product_id + " catId: " + cat_id;
+        return Ok(response);
+    }
 }
