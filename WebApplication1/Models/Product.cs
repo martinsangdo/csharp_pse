014903f801b0e2911acff1957e28de2353bd7f99
Martin Sang Do <dothanhsang1984@yahoo.com>
2025-11-16 04:20:02 +0700

    - correc type of model Product

--- WebApplication1/Models/Product.cs
+++ WebApplication1/Models/Product.cs
@@ -3,11 +3,11 @@ public class Product
     public int Id { get; set; }
     public string Name { get; set; }
     public double Price { get; set; }
-    public double description { get; set; }
-    public double image_url { get; set; }
-    public double category_id { get; set; }
+    public string description { get; set; }
+    public string image_url { get; set; }
+    public int category_id { get; set; }
 
     public double revenue { get; set; }  //this is private info that not show to user
-    public double provider_id { get; set; }  //this is private info that not show to user
+    public int provider_id { get; set; }  //this is private info that not show to user
 
 }
