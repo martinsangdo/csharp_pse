82c1f3967404189c8aac89cde7d0378156f78c29
Martin Sang Do <dothanhsang1984@yahoo.com>
2025-11-15 16:55:22 +0700

    - some POST APIs

--- WebApplication1/Controllers/API/ProductsControllerBase.cs
+++ WebApplication1/Controllers/API/ProductsControllerBase.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 
 [Route("api/product")]  //
@@ -34,4 +35,45 @@ public class ProductsControllerBase : ControllerBase
         string response = "Received product ID: " + product_id + " catId: " + cat_id;
         return Ok(response);
     }
+    //========== POST
+    //get data from Form
+    [HttpPost]
+    [Route("")]
+    public IActionResult createNewProduct(string name, string description)
+    {
+        string response = "Received name: " + name + ", description: " + description;
+        return Ok(response);
+    }
+
+    [HttpPost("raw")]
+    public async Task<IActionResult> getProductRawText()
+    {
+        using var reader = new StreamReader(Request.Body);
+        var json = await reader.ReadToEndAsync();
+        return Ok(json);
+    }
+    
+    [HttpPost("discount")]
+    public async Task<IActionResult> calculateDiscount()
+    {
+        using var reader = new StreamReader(Request.Body);
+        var json = await reader.ReadToEndAsync();
+        var data = JsonSerializer.Deserialize<Dictionary<string, object>>(json);
+        Console.WriteLine(data["price"]);
+        return Ok(data["price"]);
+    }
+    //received via Dto
+    [HttpPost]
+    public IActionResult CreateProduct([FromBody] ProductDto product)
+    {
+        return Ok(new 
+        {
+            Message = "Product received",
+            Data = product
+        });
+    }
+    //========== PUT
+
+    //========== DELETE
+
 }
