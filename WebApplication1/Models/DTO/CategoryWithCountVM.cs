88a2fd9b809890e2c9d8de578d953160ceb3303b
Martin Sang Do <dothanhsang1984@yahoo.com>
2026-01-29 20:56:48 +0700

    - show category with total of products

---
+++ WebApplication1/Models/DTO/CategoryWithCountVM.cs
@@ -0,0 +1,6 @@
+public class CategoryWithCountVM
+{
+    public int CategoryID { get; set; }
+    public string Name { get; set; }
+    public int TotalProducts { get; set; }
+}
--- WebApplication1/Services/CategoryService.cs
+++ WebApplication1/Services/CategoryService.cs
@@ -13,12 +13,33 @@ public class CategoryService
         _db = db;
     }
     //get top categories
-    public List<Category> getLeafCategories()
+    public List<CategoryWithCountVM> getLeafCategories()
     {
-        var childCategories = _db.Category
-                              //   .Where(c => c.ParentCategoryID != null)
-                              //   .Take(6)
-                              .ToList();
-        return childCategories;
+        // var categories = _db.Category
+        //                       //   .Where(c => c.ParentCategoryID != null)
+        //                       //   .Take(6)
+        //                       .ToList();
+
+        /*
+        SELECT c.Id, c.Name, COUNT(p.Id) AS TotalProducts
+        FROM Categories c
+        LEFT JOIN Products p ON p.CategoryId = c.Id
+        GROUP BY c.Id, c.Name
+        ORDER BY c.Name;
+        */
+        var categories = (from c in _db.Category
+                 join p in _db.Product on c.CategoryID equals p.CategoryId into g
+                 select new CategoryWithCountVM
+                 {
+                     CategoryID = c.CategoryID,
+                     Name = c.Name,
+                     TotalProducts = g.Count()
+                 }).ToList();
+
+        // int totalProductsInCategoryA = _db.Product
+        //     .Count(p => p.CategoryId == categoryId);
+
+
+        return categories;
     }
 }
